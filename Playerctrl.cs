17dda8c13f51ee4c07137b345ed97cb37a0ead44
thien <thienphanlac74@gmail.com>
2023-02-25 23:16:19 +0700

    test

---
+++ Playerctrl.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerctrl : MonoBehaviour
+{  
+
+   static public Playerctrl instance;
+
+   public Damagereceiver damagereceiver ;
+   public Playerstatus playerstatus;
+   protected void Awake()
+   {
+      Playerctrl.instance =this;
+     this.damagereceiver = GetComponent<Damagereceiver>();
+    this.playerstatus = GetComponent<Playerstatus>();
+
+   }
+}
